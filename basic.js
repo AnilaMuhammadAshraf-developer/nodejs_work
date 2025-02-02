e3d9cb1809cfab08266002a48fd362494de9f7f9
SoftwareDeveloper-flutter <anilamuhammadashraf.software@gmail.com>
2025-02-03 00:33:08 +0500

    node js class work

---
+++ basic.js
@@ -0,0 +1,63 @@
+// console.log("Hello World");
+
+// var a=10;
+
+// //const ki value reassigned ni hoti hai 
+// function afunction(){
+//     let b=20;
+//     const c=30;
+//     console.log(c);
+//     b=40;
+//     console.log(b);
+// }
+
+// afunction();
+
+
+// //obj define 
+// const person={
+//     name:"User",
+//     email:"user@gmail.com",
+//     phone:"09007843958",
+//     id:1
+// }
+// console.log("obj print",person);
+
+
+// //All Datatype variables console here 
+
+// a="UserString";
+// b=3;
+// c=5.3;
+// x=true;
+// const userObj={
+//     id:3,
+//     name:"Johnyy User",
+//     email:"johnny@gmail.com",
+//     marks:99.9
+    
+// }
+
+// const userArray=[
+//     "Johhny","Lucy","William","Mike"
+// ];
+
+// let xyz;
+// car = undefined;
+// const date=new Date("2025-02-31");
+// console.log("String",a);
+// console.log("int",b);
+// console.log("decimal",c);
+// console.log("boolean",x);
+// console.log("obj",userObj);
+// console.log("Array",userArray);
+// console.log("date",date);
+// console.log("this variable:",xyz);
+// console.log(typeof userArray);
+// console.log(typeof userObj);
+// console.log(typeof xyz);
+// console.log(car);
+
+// console.log("Hello World");
+
+
